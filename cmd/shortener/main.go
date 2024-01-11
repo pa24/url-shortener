d48916a5695fc3a3b06b26d5b0f69b54ec06654c
Andrey <pa24@yandex.ru>
2024-01-11 15:26:48 +0300

    rename

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -12,58 +12,50 @@ var urlStore = make(map[string]string)
 
 func main() {
 	mux := http.NewServeMux()
-	mux.HandleFunc(`/`, shortenURLHandler)
-	mux.HandleFunc(`/{id}`, redirectHandler)
+	mux.HandleFunc("/", shortenURLHandler)
 
-	err := http.ListenAndServe(`:8080`, mux)
+	err := http.ListenAndServe(":8080", mux)
 	if err != nil {
 		panic(err)
 	}
 }
 
 func shortenURLHandler(w http.ResponseWriter, r *http.Request) {
-	if r.Method != http.MethodPost {
-		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+	if r.Method == http.MethodPost {
+
+		body, err := io.ReadAll(r.Body)
+		if err != nil {
+			http.Error(w, "Error reading request body", http.StatusInternalServerError)
+			return
+		}
+
+		originalURL := string(body)
+		shortURL := generateShortURL()
+		urlStore[shortURL] = originalURL
+		response := fmt.Sprintf("http://localhost:8080/%s", shortURL)
+
+		w.WriteHeader(http.StatusCreated)
+		w.Header().Set("Content-Type", "text/plain")
+		w.Write([]byte(response))
+
+	} else if r.Method == http.MethodGet {
+		id := strings.TrimPrefix(r.URL.Path, "/")
+
+		originalURL, exists := urlStore[id]
+		if !exists {
+			http.Error(w, "Not Found", http.StatusNotFound)
+			return
+		}
+
+		http.Redirect(w, r, originalURL, http.StatusTemporaryRedirect)
+	} else {
+		http.Error(w, "only GET allowed", http.StatusMethodNotAllowed)
 		return
 	}
-
-	body, err := io.ReadAll(r.Body)
-	if err != nil {
-		http.Error(w, "Error reading request body", http.StatusInternalServerError)
-		return
-	}
-
-	originalURL := string(body)
-	shortURL := generateShortURL()
-	urlStore[shortURL] = originalURL
-	response := fmt.Sprintf("http://localhost:8080/%s\n", shortURL)
-
-	w.WriteHeader(http.StatusCreated)
-	w.Header().Set("Content-Type", "text/plain")
-	//w.Header().Set("Content-Length", fmt.Sprint(len(response)))
-
-	w.Write([]byte(response))
 }
 
 func generateShortURL() string {
 	id := uuid.New().String()
-	// Remove dashes to make it more URL-friendly
-	return strings.ReplaceAll(id, "-", "")
-}
-
-func redirectHandler(w http.ResponseWriter, r *http.Request) {
-	if r.Method != http.MethodGet {
-		http.Error(w, "only GET allow", http.StatusMethodNotAllowed)
-	}
-	id := strings.TrimPrefix(r.URL.Path, "/")
-
-	originalURL, exists := urlStore[id]
-	if !exists {
-		http.Error(w, "Not Found", http.StatusNotFound)
-		return
-	}
-
-	w.Header().Set("Location", originalURL)
-	w.WriteHeader(http.StatusTemporaryRedirect)
-
+	strings.ReplaceAll(id, "-", "")
+	return id[:6]
 }
